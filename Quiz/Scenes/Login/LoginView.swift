73dfba20b8d01ed52168978f16b20901b4689063
ilhan <ilhanodun34@gmail.com>
2023-11-06 08:10:12 +0300

    Created basic login page

---
+++ Quiz/Scenes/Login/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  Quiz
+//
+//  Created by Ilhan ODUN on 6.11.2023.
+//
+
+import Foundation
---
+++ Quiz/Scenes/Quiz/QuizStartView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizStartView.swift
+//  Quiz
+//
+//  Created by Ilhan ODUN on 6.11.2023.
+//
+
+import SwiftUI
+
+struct QuizStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizStartView()
+}
